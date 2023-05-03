37be86513bed4e6f6f07bb5b48a0caa0076b9006
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-03 15:23:16 +0200

    add second word ***

--- myscript.php
+++ myscript.php
@@ -1,7 +1,11 @@
 <?php
-
+// calcolo lunghezza della seconda stringa 'cognome'
 $lunghezzaInput = strlen($_POST['cognome']);
 
-echo 'Ciao '. $_POST['nome'] .' ' .$lunghezzaInput .' ';
+//sostituisco alle lettere gli *
+$replace = str_repeat('*', $lunghezzaInput);
 
+//modificare la seconda parola
+$nuovoTesto = str_replace($_POST['cognome'], $replace, $_POST['cognome']);
 
+echo 'Ciao '. $_POST['nome'] .' ' .$nuovoTesto;
